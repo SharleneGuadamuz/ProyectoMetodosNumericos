689fff49bf960ee02e80011153ec169691df171f
SharleneGuadamuz <90294857+SharleneGuadamuz@users.noreply.github.com>
2023-06-29 09:04:27 -0600

    creacion de ProyectoInterpolacion

---
+++ ProyectoInterpolacion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoInterpolacion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
